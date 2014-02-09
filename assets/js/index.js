20f2b310d545c41a298a71fddb230eae9b2081db
dingledow <david@ingledow.co.uk>
2014-02-09 12:21:39 +0000

    * Added author to posts * Fixed nav * Fixed responsiveness * Implemented extract that works (hacky)

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function() {
+
+        $('.post-excerpt p:nth-child(2)').css("display", "none");
+
+    });
+
+}(jQuery));
